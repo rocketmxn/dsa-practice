e4c6fbce24ba4ec3946204eee06abc46b7b95566
Kshitij Sharma <63995641+kshitij-codes@users.noreply.github.com>
2025-06-06 12:07:22 +0530

    Time: 50 ms (16.63%) | Memory: 62.3 MB (13.84%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.js
+++ 76-minimum-window-substring/minimum-window-substring.js
@@ -4,37 +4,37 @@
  * @return {string}
  */
 var minWindow = function(s, t) {
-    if(s.length < t.length) return ""
-    const charCount = new Map()
-    for(let ch of t) {
-        charCount.set(ch, (charCount.get(ch) || 0) + 1)
-    }
-    let targetCharsRemaining = t.length
-    let minWindow = [0, Number.POSITIVE_INFINITY]
-    let startIndex = 0
+   if(s.length < t.length) return ""
+   const charCount = new Map()
+   for(let char of t) {
+    charCount.set(char, (charCount.get(char) || 0) + 1)
+   }
+   let targetCharsRemaining = t.length
+   let minWindow = [0, Number.POSITIVE_INFINITY]
+   let startIndex = 0
 
-    for(let endIndex=0; endIndex < s.length; endIndex++) {
-        let currentChar = s[endIndex]
-        if(charCount.has(currentChar) && charCount.get(currentChar) > 0) {
-            targetCharsRemaining--
-        }
-        charCount.set(currentChar, (charCount.get(currentChar) || 0) - 1)
-        if(targetCharsRemaining === 0) {
-            while(true) {
-                const charAtStart = s[startIndex]
-                if(charCount.has(charAtStart) && charCount.get(charAtStart) === 0) {
-                    break
-                }
-                charCount.set(charAtStart, (charCount.get(charAtStart) || 0) + 1)
-                startIndex++
+   for(let endIndex = 0; endIndex < s.length; endIndex++) {
+    let currentChar = s[endIndex]
+    if(charCount.has(currentChar) && charCount.get(currentChar) > 0) {
+        targetCharsRemaining--
+    }
+    charCount.set(currentChar, (charCount.get(currentChar) || 0) - 1)
+    if(targetCharsRemaining === 0) {
+        while(true) {
+            const charAtStart = s[startIndex]
+            if(charCount.has(charAtStart) && charCount.get(charAtStart) === 0) {
+                break
             }
-            if(endIndex - startIndex < minWindow[1] - minWindow[0]) {
-                minWindow = [startIndex, endIndex]
-            }
-            charCount.set(s[startIndex], (charCount.get(s[startIndex] || 0) + 1))
-            targetCharsRemaining++
+            charCount.set(charAtStart, (charCount.get(charAtStart) || 0) + 1 )
             startIndex++
         }
+        if(endIndex - startIndex < minWindow[1] - minWindow[0]) {
+            minWindow = [startIndex, endIndex]
+        }
+        charCount.set(s[startIndex], (charCount.get(s[startIndex]) || 0) + 1)
+        targetCharsRemaining++
+        startIndex++
     }
-   return minWindow[1] >= s.length ? "" : s.slice(minWindow[0], minWindow[1] + 1)
+   }
+   return minWindow[1] === Number.POSITIVE_INFINITY ? "" : s.slice(minWindow[0], minWindow[1] + 1)
 };
