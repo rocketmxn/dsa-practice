db61e49379a5f987ec0353c5dbf1e5b0d3560f06
Kshitij Sharma <63995641+kshitij-codes@users.noreply.github.com>
2025-06-12 21:22:00 +0530

    Time: 57 ms (76.52%) | Memory: 58.8 MB (68.47%) - LeetSync

--- 200-number-of-islands/number-of-islands.js
+++ 200-number-of-islands/number-of-islands.js
@@ -3,27 +3,28 @@
  * @return {number}
  */
 var numIslands = function(grid) {
-    if(!grid || grid.length === 0) return 0
-    const numRows = grid.length
-    const numCols = grid[0].length
-    let islandCount = 0
-    function dfs (r, c) {
-        if(r<0 || r >= numRows) return
-        if(c<0 || c >= numCols) return
+   if(!grid || !grid.length) return 0
+   const rows = grid.length
+   const cols = grid[0].length
+   let islandCount = 0
+
+    function dfs(r, c) {
+        if(r<0 || c<0 || r>=rows || c>=cols) return 
         if(grid[r][c] !== "1") return
         grid[r][c] = "#"
-        dfs(r-1, c)
-        dfs(r+1, c)
-        dfs(r, c+1)
         dfs(r, c-1)
+        dfs(r, c+1)
+        dfs(r+1, c)
+        dfs(r-1, c)
     }
-    for(let i=0; i<numRows; i++) {
-        for(let j=0; j<numCols; j++) {
-            if(grid[i][j] === "1") {
-                dfs(i, j)
-                islandCount++
-            }
+
+   for(let i=0; i<rows; i++) {
+    for(let j=0; j<cols; j++) {
+        if(grid[i][j] === "1") {
+            dfs(i, j)
+            islandCount++
         }
     }
-    return islandCount
+   }
+   return islandCount
 };
