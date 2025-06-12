f62908efe647e26309ab5912b6c4ae4bb07906b8
Kshitij Sharma <63995641+kshitij-codes@users.noreply.github.com>
2025-06-12 21:11:37 +0530

    Time: 0 ms (100.00%) | Memory: 59 MB (31.64%) - LeetSync

--- 733-flood-fill/flood-fill.js
+++ 733-flood-fill/flood-fill.js
@@ -5,21 +5,21 @@
  * @param {number} color
  * @return {number[][]}
  */
-var floodFill = function(image, sr, sc, newColor) {
-    if(image[sr][sc] === newColor) {
-        return image
-    }
-    const rowLength = image.length
-    const colLength = image[0].length
+var floodFill = function (image, sr, sc, newColor) {
+    if(image[sr][sc] === newColor) return image
+    const rows = image.length
+    const cols = image[0].length
     const originalColor = image[sr][sc]
-    function DFS(r, c) {
-        if(r>=rowLength || c>=colLength || r<0 || c<0 || image[r][c] !== originalColor) return
+
+    function dfs(r,c){
+        if(r<0 || r>=rows || c>=cols || c<0 || image[r][c] !== originalColor ) return
         image[r][c] = newColor
-        DFS(r+1, c)
-        DFS(r-1, c)
-        DFS(r, c+1)
-        DFS(r, c-1)
+        dfs(r, c+1)
+        dfs(r, c-1)
+        dfs(r+1, c)
+        dfs(r-1, c)
     }
-    DFS(sr, sc)
+
+    dfs(sr, sc)
     return image
-};
+}
