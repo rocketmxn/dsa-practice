c106032e011115a7ee3472ea85af7b050239dfe0
Kshitij Sharma <63995641+kshitij-codes@users.noreply.github.com>
2025-06-12 12:39:04 +0530

    Time: 1 ms (86.59%) | Memory: 58.6 MB (57.87%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.js
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.js
@@ -11,19 +11,19 @@
  * @return {number[][]}
  */
 var levelOrder = function(root) {
-   let res = []
-   if(!root) return res
-   let q = [root]
-   while(q.length > 0) {
-    let sameLevel = []
-    let levelSize = q.length
-    for(let i=0; i<levelSize; i++) {
-        const node = q.shift()
-        sameLevel.push(node.val)
-        if(node.left) q.push(node.left)
-        if(node.right) q.push(node.right)
-    }
+    if(!root) return []
+    let res = []
+    let q = [root]
+    while(q.length !== 0) {
+        let sameLevel = []
+        let levelSize = q.length
+        for(let i=0; i<levelSize; i++) {
+            const node = q.shift()
+            sameLevel.push(node.val)
+            if(node.left) q.push(node.left)
+            if(node.right) q.push(node.right)
+        }
         res.push(sameLevel)
-   }
+    }
     return res
 };
