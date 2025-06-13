8dcd779318208a7dceced72059b32549132d5802
Kshitij Sharma <63995641+kshitij-codes@users.noreply.github.com>
2025-06-13 09:09:09 +0530

    Time: 0 ms (100.00%) | Memory: 53.7 MB (31.78%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.js
+++ 70-climbing-stairs/climbing-stairs.js
@@ -2,13 +2,13 @@
  * @param {number} n
  * @return {number}
  */
-var climbStairs = function(n) {
+var climbStairs = function (n) {
     let secondLast = 1
     let last = 1
-    for(let i=1; i<n; i++) {
-        const temp = secondLast
-        secondLast = secondLast + last
-        last = temp
+    for(let i=1;i<n;i++){
+        const temp = last
+        last += secondLast
+        secondLast = temp
     }
-    return secondLast
-};
+    return last
+}
