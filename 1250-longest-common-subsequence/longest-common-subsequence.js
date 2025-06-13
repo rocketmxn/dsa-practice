5a879df9307014d79e2fc1850f991006628c287c
Kshitij Sharma <63995641+kshitij-codes@users.noreply.github.com>
2025-06-13 11:21:55 +0530

    Time: 42 ms (75.21%) | Memory: 86.1 MB (30.90%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.js
+++ 1250-longest-common-subsequence/longest-common-subsequence.js
@@ -4,15 +4,15 @@
  * @return {number}
  */
 var longestCommonSubsequence = function(text1, text2) {
-    let m = text1.length
+    let m  = text1.length
     let n = text2.length
-    let dp = Array.from({length: m+n}, () => new Array(n+1).fill(0))
+    let dp = Array.from({length: m+1}, () => new Array(n+1).fill(0))
     for(let i=1; i<=m; i++) {
         for(let j=1; j<=n; j++) {
-            if(text1[i-1] === text2[j-1]) {
+            if(text1[i-1] === text2[j-1]){
                 dp[i][j] = dp[i-1][j-1] + 1
             } else {
-                dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1])
+                dp[i][j] = Math.max(dp[i][j-1], dp[i-1][j])
             }
         }
     }
