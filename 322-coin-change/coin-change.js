856add43f101829e15fd20b9f2b6ca28933e3ce5
Kshitij Sharma <63995641+kshitij-codes@users.noreply.github.com>
2025-06-13 09:48:13 +0530

    Time: 48 ms (46.69%) | Memory: 59 MB (78.66%) - LeetSync

--- 322-coin-change/coin-change.js
+++ 322-coin-change/coin-change.js
@@ -6,10 +6,10 @@
 var coinChange = function(coins, amount) {
     let minCoins = new Array(amount+1).fill(amount+1)
     minCoins[0] = 0
-    for(let a=1; a<=amount; a++) {
+    for(let a=1;a<=amount;a++){
         for(let coin of coins) {
-            if(a-coin >=0) {
-                minCoins[a] = Math.min(minCoins[a], 1 + minCoins[a - coin])
+            if(a-coin >= 0) {
+                minCoins[a] = Math.min(minCoins[a], 1 + minCoins[a-coin])
             }
         }
     }
