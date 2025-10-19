0c812adba4b6a300b6b0dfc0e735db25771dcedb
Kshitij Sharma <63995641+rocketmxn@users.noreply.github.com>
2025-10-19 20:42:38 +0530

    Time: 1 ms (79.01%) | Memory: 55.4 MB (57.18%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.js
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.js
@@ -4,23 +4,14 @@
  * @return {number[]}
  */
 var intersection = function(nums1, nums2) {
-    let i = 0
-    let j = 0
-const newArr = []
-const newNums1 = nums1.sort((a,b)=> a-b )
-const newNums2 = nums2.sort((a,b)=> a-b )
-    while(i<newNums1.length  && j<newNums2.length) {
-        if(newNums1[i] < newNums2[j]) {
-            i++
-        } else if (newNums1[i] > newNums2[j]) {
-            j++
-        } else {
-            if(newArr[newArr.length - 1] !== newNums1[i]) {
-                newArr.push(newNums1[i])
-            }
-            i++
-            j++
+    const set1 = new Set(nums1)
+    const set2 = new Set(nums2)
+    const result = []
+
+    for(let num of set1) {
+        if(set2.has(num)) {
+            result.push(num)
         }
     }
-    return newArr
+    return result
 };
