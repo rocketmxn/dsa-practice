88121f652d9103295980ea03c4d45a6c2a46ea14
Kshitij Sharma <63995641+rocketmxn@users.noreply.github.com>
2025-10-19 20:30:55 +0530

    Time: 2 ms (78.68%) | Memory: 63.7 MB (32.42%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.js
+++ 11-container-with-most-water/container-with-most-water.js
@@ -1,19 +1,23 @@
 /**
- * @param {number[]} height
+ * @param {number[]} walls
  * @return {number}
  */
-var maxArea = function(height) {
-    let left = 0
-    let right = height.length - 1
-    let maxArea = 0
+var maxArea = function(walls) {
+   let left = 0
+  let right = walls.length - 1
+  let maxArea = 0
 
-    while(left < right) {
-        maxArea = Math.max(maxArea, (right-left) * Math.min(height[left], height[right]))
-        if(height[left] < height[right]) {
-            left++
-        } else {
-            right--
-        }
+  while(left < right) {
+    const height = Math.min(walls[left], walls[right])
+    const width = right - left
+    const area = height * width
+    maxArea = Math.max(maxArea, area)
+
+    if(walls[left] < walls[right]) {
+      left++
+    } else {
+      right--
     }
-    return maxArea
+  }
+  return maxArea
 };
