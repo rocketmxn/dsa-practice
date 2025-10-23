b3322955edc47e94b4e3ae92998dbfcd21ae4890
Kshitij Sharma <63995641+rocketmxn@users.noreply.github.com>
2025-10-24 02:04:35 +0530

    Time: 5 ms (36.84%) | Memory: 57.8 MB (24.73%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.js
+++ 20-valid-parentheses/valid-parentheses.js
@@ -3,22 +3,15 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    const stackOfOpenBrackets = [] // closing Brackets
-    const mapping = {
-        ')': '(',
-        '}': '{',
-        ']': '['
+   const openFor = {")":"(", "}":"{", "]":"["}
+   const stack = []
+   for(const ch of s) {
+    if(ch === "(" || ch === "{" || ch === "[") {
+        stack.push(ch)
     }
-    for (const char of s) {
-      if(Object.values(mapping).includes(char)) { // if open bracket
-        stackOfOpenBrackets.push(char)
-      } else if (mapping.hasOwnProperty(char)) { // if close bracket
-        if(!stackOfOpenBrackets.length || mapping[char] !== stackOfOpenBrackets.pop()) {
-            return false
-        }
-      }
+    if(ch === ")" || ch === "}" || ch === "]") {
+        if(stack.pop() !== openFor[ch]) return false
     }
-
-    // If the stack is empty at the end, all brackets were matched correctly.
-    return stackOfOpenBrackets.length === 0;
+   }
+   return stack.length === 0
 };
